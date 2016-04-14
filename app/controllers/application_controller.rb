4bfe9ac9563ee7a8030bdd8d8402ae60dbc0bb89
Adriano Godoy <godoy.ccp@gmail.com>
2016-04-14 13:10:00 -0300

    Redirect to home when not authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to root_path, :notice => 'You need login first.'
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
+
 end
