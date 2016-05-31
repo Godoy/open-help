ece3c8ddbf1a945f36131abc0a399e10a6648c78
Adriano Godoy <godoy.ccp@gmail.com>
2016-05-30 22:14:19 -0300

    Move counters from helper to partial. Changes repo icons to github svg

--- app/helpers/repo_helper.rb
+++ app/helpers/repo_helper.rb
@@ -1,23 +1,4 @@
 module RepoHelper
-  def repo_counters(repo)
-    content = "<span class='label label-warning' title='Stars'>" +
-              "<span class='glyphicon glyphicon-star' aria-hidden='true'></span> " +
-              "#{repo.github_stargazers_count}" +
-              "</span>" if repo.github_stargazers_count.present?
-
-    content += "<span class='label label-info' title='Forks'>" +
-              "<span class='glyphicon glyphicon-transfer' aria-hidden='true'></span> " +
-              "#{repo.github_forks}" +
-              "</span>" if repo.github_forks.present?
-
-    content += "<span class='label label-danger' title='Issues'>" +
-              "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> " +
-              "#{repo.github_open_issues}" +
-              "</span>" if repo.github_open_issues.present?
-
-    content.html_safe
-  end
-
   def link_add_or_edit_repo(repo)
     if @registered_repos.include? repo.id.to_s
       render "repos/link_edit", repo: repo
